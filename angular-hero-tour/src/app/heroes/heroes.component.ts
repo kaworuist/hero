0e434383d4cffcf659958bf1c03cedad88e61eb2
kaworu <2575961910@qq.com>
2021-04-16 17:23:00 +0800

    part2

--- angular-hero-tour/src/app/heroes/heroes.component.ts
+++ angular-hero-tour/src/app/heroes/heroes.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core'
-import { Hero } from '../hero'
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { heroes } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'kaworu'
-  }
+  heroes = heroes;
+  selectedHero: Hero|undefined;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void{
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
